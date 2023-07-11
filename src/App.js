140730cad492b949600fb53e70a5a8db23fca8e1
Elena Anosova <anosova.helena3@gmail.com>
2023-07-11 17:52:42 +0200

    added info on mockAPI, made card component and card scss module, some stuff with buttons etc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React from 'react'
 import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
 function App() {
+  const [items, setItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([])
+  const [cartOpened, setCartOpened] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch('https://64ad5f16b470006a5ec5d9d5.mockapi.io/items')
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        setItems(json)
+      })
+  }, [])
+
+  const onAddToCart = (obj) => {
+    setCartItems((prev) => [...prev, obj])
+  }
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="mb-40 d-flex justify-between align-center">
           <h1>Все платья</h1>
@@ -15,11 +36,17 @@ function App() {
             <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
